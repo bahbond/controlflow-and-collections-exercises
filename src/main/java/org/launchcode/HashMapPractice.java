ee5d85cb574fb709312b9c4eba58d654f665f636
bahbond <bondopicks@gmail.com>
2023-08-30 14:27:16 -0500

    HashMap p

--- src/main/java/org/launchcode/HashMapPractice.java
+++ src/main/java/org/launchcode/HashMapPractice.java
@@ -1,35 +1,35 @@
 package org.launchcode;
+
 import java.util.HashMap;
 import java.util.Scanner;
 import java.util.Map;
 public class HashMapPractice {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        HashMap<String, Integer> students = new HashMap<>();
-        Scanner input = new Scanner(System.in);
-        String newStudent;
+        HashMap<Integer, String> students = new HashMap<>();
+        Scanner input = new Scanner (System.in);
+        Integer studentId;
 
-        System.out.println("Enter your students name");
+        //get Id and Name
 
-        // Get Student names and IDS
-         do{
-             System.out.println("Student: ");
-             newStudent = input.nextLine();
+        do{
+            System.out.println("Student Id: ");
+            studentId = input.nextInt();
+//PROBLEM - CONDITIONAL NEEDED FOR EMPTY INTEGER NEXT LINE
+            if (!studentId.equals("")){
+                System.out.println(("Student Name: "));
+                String studentName = input.nextLine();
+                students.put(studentId,studentName);
 
-             if(!newStudent.equals("")){
-                 System.out.println("Student ID ");
-                 Integer studentID = input.nextInt();
-                 students.put(newStudent, studentID);
+                // Read in the newline before looping back
+                input.nextLine();
+            }
+        }while(!studentId.equals(""));
+        // Print Student List
+        System.out.println("\nStudent List");
 
-                 input.nextLine();
-             }
-         }while(!newStudent.equals(""));
-        System.out.println("Class List; ");
-
-        for (Map.Entry<String, Integer> student : students.entrySet()){
-            System.out.println(student.getKey() + "(" +student.getValue() + ")");
+        for(Map.Entry<Integer,String> ids : students.entrySet()){
+            System.out.println(ids.getKey() + "(" + ids.getValue() +")");
         }
-        System.out.println(students);
     }
-
 }
